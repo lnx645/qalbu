b24acf16e78a64d08fa1ae53ab259451e66f2ba8
d03x <dadanhidyt@gmail.com>
2026-01-21 22:12:01 +0700

    feat: initialize Nuxt 4 project with Tailwind CSS, add basic API endpoint, and configure TypeScript

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="max-w-2xl bg-white min-h-screen mx-auto">
+    <!-- BEGIN:HEADER -->
+    <header class="bg-white shadow h-14">DAS</header>
+    <!-- END:HEADER -->
+    <slot />
+  </div>
+</template>
